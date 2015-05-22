161a18609517ca4a0aef92fe4f49ae2e66c87ce6
CatPhat <kian@msoon.com>
2015-05-22 16:52:27 -0700

    add Requests directory and associated requests

---
+++ Requests/PartsRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Octosharp.Requests
+{
+    //example: http://octopart.com/api/v3/parts/103cdb613d20cffb?apikey=APIKEYGOESHERE
+    public class PartsRequest : IOctoRequest<>
+    {
+        private readonly string _endpoint;
+
+        public PartsRequest(string endpoint)
+        {
+            if (endpoint == null) throw new NullReferenceException("endpoint");
+            _endpoint = endpoint;
+        }
+
+        public string PathSegment
+        {
+            get { return "parts/" + _endpoint; }
+        }
+
+        public IEnumerable<QueryParameter> QueryParmeters { get; private set; }
+    }
+}
---
+++ Requests/QueryParameter.cs
@@ -0,0 +1,24 @@
+﻿namespace Octosharp.Requests
+{
+    public class QueryParameter
+    {
+        private readonly string _name;
+        private readonly string _value;
+
+        internal QueryParameter(string name, string value)
+        {
+            _name = name;
+            _value = value;
+        }
+
+        internal string Name
+        {
+            get { return _name; }
+        }
+
+        internal string Value
+        {
+            get { return _value; }
+        }
+    }
+}
