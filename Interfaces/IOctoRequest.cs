75c129edae03be209e9fd57927a0b197e91447e3
CatPhat <kian@msoon.com>
2015-05-22 16:52:04 -0700

    Add interfaces directory and associated interface

---
+++ Interfaces/IOctoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Octosharp.Requests;
+using Octosharp.Responses;
+
+namespace Octosharp
+{
+    public interface IOctoRequest<TResponse> where TResponse : OctoJsonResponse
+    {
+        string PathSegment { get; }
+        IEnumerable<QueryParameter> QueryParmeters { get; }
+    }
+}
