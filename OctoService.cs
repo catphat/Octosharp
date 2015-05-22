9cc50173028fdfb93faaf1c4cf83842cbc80ccc6
CatPhat <kian@msoon.com>
2015-05-22 16:54:10 -0700

    Add OctoService class

---
+++ OctoService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Octosharp
+{
+    public class OctoService
+    {
+        private readonly string _apiKey;
+        private readonly string _baseUrl;
+
+        public OctoService(string baseUrl, string apiKey)
+        {
+            if (baseUrl == null) throw new NullReferenceException("baseUrl");
+            if (apiKey == null) throw new NullReferenceException("apiKey");
+
+            _baseUrl = baseUrl;
+            _apiKey = apiKey;
+        }
+
+        public async Task<string> GetStringAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
