4f865d320314f17917e8f7b10f48722fe97869e2
CatPhat <kian@msoon.com>
2015-05-22 16:51:46 -0700

    Add Helpers directory and associated enum helpers.

---
+++ Helpers/OctoEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octosharp.Helpers
+{
+    public enum PartsEndpoint
+    {
+        [RequestValue("match")] Match,
+        [RequestValue("search")] Search,
+        [RequestValue("get_multi")] GetMultiple
+    }
+}
---
+++ Helpers/RequestValueEnumAttributes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Octosharp.Helpers
+{
+    internal class RequestValue : Attribute
+    {
+        private readonly string _value;
+
+        public RequestValue(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+    }
+
+    internal static class GetRequestValueAttribute
+    {
+        internal static string GetStringValue(this Enum value)
+        {
+            string output = null;
+            var type = value.GetType();
+            var field = type.GetField(value.ToString());
+            var attributes = field.GetCustomAttributes(typeof (RequestValue), false) as RequestValue[];
+            if (attributes != null && attributes.Length > 0)
+            {
+                output = attributes[0].Value;
+            }
+            return output;
+        }
+    }
+}
